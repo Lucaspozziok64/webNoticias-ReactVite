c926547583a7ef76784c52a580219152674c6775
Lucasfi <lukafigueroa64@gmail.com>
2025-07-02 14:12:21 -0300

    agreegamos una variable de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   const obtenerNoticia = async () => {
     try {
       const respuesta = await fetch(
-        `https://newsdata.io/api/1/latest?apikey=pub_7c1484730fa64707bd2289a5f123fd36&category=${categoria}&language=es`
+        `https://newsdata.io/api/1/latest?apikey=&category=${categoria}&language=es`
       );
       const datos = await respuesta.json()
       setNoticia(datos.results || [])
